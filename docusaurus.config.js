ce7cb5d35bb853291633bfe65c0aa5716da5dc6e
Oleksandr Lisovytskyi <alexandr.lisovitsky@gitlab.webamg.com>
2023-04-19 08:55:45 +0300

    Update config file with a correct repo links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ module.exports = {
             },
             {
               label: 'Repository',
-              href: 'https://github.com/Lisalexandr',
+              href: 'https://github.com/Lisalexandr/documentation',
             },
           ],
         },
